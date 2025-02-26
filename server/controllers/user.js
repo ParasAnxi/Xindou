c1d656a9d997329eed03e39c2e42b03da345fe64
Paras Bhandari <paras.bhandari7010@gmail.com>
2025-02-26 13:54:27 +0530

    Get user profile restAPI
    API to find user profile using user name.

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -18,3 +18,18 @@ export const searchUser = async (req, res) => {
     res.status(401).json({ error: error });
   }
 };
+//** USER PROFILE */
+export const getUserProfile = async (req, res) => {
+  try {
+    const { userName } = req.body;
+    const user = await User.findOne({ userName: userName });
+    if (!user) res.status(404).json({ error: "User Not Found!" });
+    user.email = undefined;
+    user.password = undefined;
+    user.resetPasswordToken = undefined;
+    user.notifications = undefined;
+    res.status(200).json({ user: user });
+  } catch (error) {
+    res.status(404).status({ error: error });
+  }
+};
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,11 +1,12 @@
 //** IMPORTS */
 import express from "express";
-import { searchUser } from "../controllers/user.js";
+import { searchUser, getUserProfile } from "../controllers/user.js";
 
 //** CONFIG */
 const router = express.Router();
 
 //** GET USERS */
 router.post("/findusers",searchUser);
+router.post("/userprofile", getUserProfile);
 
 export default router;
